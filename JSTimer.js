42bc84c7c6f3341c7adaf17473bf64f5cc632f17
MussabElDash <mussab14@gmail.com>
2014-07-21 20:40:58 +0200

    add methods

--- JSTimer.js
+++ JSTimer.js
@@ -1,21 +1,21 @@
 if (typeof jQuery === 'undefined') { throw new Error('JSTimer requires jQuery') }
 
-(function ($, window) {
+(function ($, window, undefined) {
 	"use strict";
 
-	window.Timer = function (functionToBeCalled, milliseconds, options) {
+	window.JSTimer = function (functionToBeCalled, milliSeconds, options) {
 
-		if( !(this instanceof Timer) )
-			return new Timer(functionToBeCalled, milliseconds, options);
+		if( !(this instanceof JSTimer) )
+			return new JSTimer(functionToBeCalled, milliSeconds, options);
 
 		var options = options || {};
-		var millisecondsStart = options["milliseconds"] || 0;
+		var milliSecondsStart = options["milliSeconds"] || 0;
 		var secondsStart = options["seconds"] || 0;
 		var minutesStart = options["minutes"] || 0;
 		var hoursStart = options["hours"] || 0;
 		var step = options["step"] || 1000;
-		var $milliseconds = $(options["millisecondsSelector"] || "");
-		var $pureMilliseconds = $(options["pureMillisecondsSelector"] || "");
+		var $milliSeconds = $(options["milliSecondsSelector"] || "");
+		var $pureMilliSeconds = $(options["pureMilliSecondsSelector"] || "");
 		var $seconds = $(options["secondsSelector"] || "");
 		var $pureSeconds = $(options["pureSecondsSelector"] || "");
 		var $minutes = $(options["minutesSelector"] || "");
@@ -23,7 +23,7 @@ if (typeof jQuery === 'undefined') { throw new Error('JSTimer requires jQuery') 
 		var $hours = $(options["hoursSelector"] || "");
 		var onInverseStop = options["onInverseStop"] || function(){};
 		var leadingZero = options["leadingZero"] || true;
-		this.milliseconds = millisecondsStart + secondsStart * 1000 + minutesStart * 60000 + hoursStart * 3600000;
+		this.milliSecond = milliSecondsStart + secondsStart * 1000 + minutesStart * 60000 + hoursStart * 3600000;
 
 		function pad(n, r) {
 			var number = parseInt(1 + Array(r).join("0"), 10);
@@ -31,42 +31,39 @@ if (typeof jQuery === 'undefined') { throw new Error('JSTimer requires jQuery') 
 		}
 
 		var increment = function () {
-			this.milliseconds += step;
+			this.milliSecond += step;
 			setTimeToElements.call(this);
-			if( (this.milliseconds + millisecondsStart) % milliseconds == 0){
+			if( (this.milliSecond + milliSecondsStart) % milliSeconds == 0){
 				functionToBeCalled.call(this);
 			}
 		}
 
 		var decrement = function () {
-			this.milliseconds -= step;
+			this.milliSecond -= step;
 			setTimeToElements.call(this);
-			if ( (millisecondsStart - this.milliseconds) % milliseconds == 0){
+			if ( (milliSecondsStart - this.milliSecond) % milliSeconds == 0){
 				functionToBeCalled.call(this);
 			}
-			if(this.milliseconds == 0){
+			if(this.milliSecond == 0){
 				clearInterval(this.timer);
 				onInverseStop.call(this);
 			}
 		}
 
 		var setTimeToElements = function(){
-			var milliseconds = this.milliseconds;
-			var hours = Math.floor(milliseconds / 3600000);
-			milliseconds = milliseconds % 3600000
-			var minutes = Math.floor(milliseconds / 60000);
-			milliseconds = milliseconds % 60000;
-			var seconds = Math.floor(milliseconds / 1000);
-			milliseconds = milliseconds % 1000;
-			$milliseconds.text(leadingZero ? pad(milliseconds, 3) : milliseconds);
+			var hours = this.hours();
+			var minutes = this.minutes();
+			var seconds = this.seconds();
+			var milliSeconds = this.milliSeconds();
+			var pureMinutes = this.pureMinutes();
+			var pureSeconds = this.pureSeconds();
+			$milliSeconds.text(leadingZero ? pad(milliSeconds, 3) : milliSeconds);
 			$seconds.text(leadingZero ? pad(seconds, 2) : seconds);
 			$minutes.text(leadingZero ? pad(minutes, 2) : minutes);
 			$hours.text(leadingZero ? pad(hours, 2) : hours );
-			var pureMinutes = Math.floor(this.milliseconds / 60000);
 			$pureMinutes.text(leadingZero ? pad(pureMinutes, 2) : pureMinutes);
-			var pureSeconds = Math.floor(this.milliseconds / 1000);
 			$pureSeconds.text(leadingZero ? pad(pureSeconds, 2) : pureSeconds);
-			$pureMilliseconds.text(leadingZero ? pad(this.milliseconds, 3) : this.milliseconds);
+			$pureMilliSeconds.text(leadingZero ? pad(this.milliSecond, 3) : this.milliSecond);
 		}
 
 		this.inverseStart = function () {
@@ -81,6 +78,125 @@ if (typeof jQuery === 'undefined') { throw new Error('JSTimer requires jQuery') 
 			this.timer = setInterval(function(){
 				increment.call(contextThis);
 			}, step);
+		}
+
+		this.milliSecondSelector = function (selector) {
+			if(selector == undefined)
+				return $milliSecondsSelector.selector;
+			$milliSecondsSelector = $(selector);
+			return this;
+		}
+
+		this.secondsSelector = function (selector){
+			if(selector == undefined)
+				return $secondsSelector.selector;
+			$secondsSelector = $(selector);
+			return this;
+		}
+
+		this.minutesSelector = function (selector){
+			if(selector == undefined)
+				return $minutesSelector.selector;
+			$minutesSelector = $(selector);
+			return this;
+		}
+
+		this.hoursSelector = function (selector){
+			if (selector == undefined)
+				return $hoursSelector.selector;
+			$hoursSelector = $(selector);
+			return this;
+		}
+
+		this.pureMilliSecondsSelector = function (selector){
+			if (selector == undefined)
+				return $pureMilliSecondsSelector.selector;
+			$pureMilliSecondsSelector = $(selector);
+			return this;
+		}
+
+		this.pureSecondsSelector = function (selector){
+			if (selector == undefined)
+				return $pureSecondsSelector.selector;
+			$pureSecondsSelector = $(selector);
+			return this;
+		}
+
+		this.pureMinutesSelector = function (selector){
+			if (selector == undefined)
+				return $pureMinutesSelector.selector;
+			$pureMinutesSelector = $(selector);
+			return this;
+		}
+
+		this.onInverseStop = function (theFunction){
+			if(theFunction == undefined)
+				return onInverseStop;
+			onInverseStop = theFunction;
+			return this;
+		}
+
+		this.leadingZero = function (bool){
+			if(bool == undefined)
+				return leadingZero;
+			leadingZero = bool;
+			return this;
+		}
+
+		this.milliSeconds = function (milli){
+			if(milli == undefined)
+				return this.milliSecond % 1000;
+			var tempMilli1 = this.milliSecond % 1000;
+			var tempMilli2 = tempMilli1 * 1000;
+			this.milliSecond = tempMilli2 + milli % 1000;
+			return this;
+		}
+
+		this.seconds = function (secs){
+			if(secs == undefined)
+				return Math.floor(this.milliSecond / 1000) % 60;
+			var tempSecs1 = this.milliSecond - this.seconds() * 1000;
+			var tempSecs2 = seconds % 60;
+			this.milliSecond = tempSecs1 + tempSecs2 * 1000;
+			return this;
+		}
+
+		this.minutes = function (mins){
+			if(mins == undefined)
+				return Math.floor(this.milliSecond / 60000) % 60;
+			var tempMins1 = this.milliSecond - this.minutes() * 60000;
+			var tempMins2 = mins % 60;
+			this.milliSecond = tempMilli1 + tempMins2 * 60000;
+			return this;
+		}
+
+		this.hours = function (hs){
+			if(hs == undefined)
+				return Math.floor(this.milliSecond / 3600000);
+			var temph = this.milliSecond - this.hours() * 3600000;
+			this.milliSecond = temph + hs * 3600000;
+			return this;
+		}
+
+		this.pureMilliSeconds = function (ms){
+			if(ms == undefined)
+				return this.milliSecond;
+			this.milliSecond = ms;
+			return this;
+		}
+
+		this.pureSeconds = function (sec){
+			if(sec == undefined)
+				return Math.floor(this.milliSecond / 1000);
+			this.milliSecond = sec * 1000;
+			return this;
+		}
+
+		this.pureMinutes = function (min){
+			if(min == undefined)
+				return Math.floor(this.milliSecond / 60000);
+			this.milliSecond = min * 60000;
+			return this;
 		}
 	}
 })(jQuery, window);
